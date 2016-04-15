ce8c8486b6e5e5e1c194e6ec8815ee2baceb079f
conanak99 <huyhoang8a5@gmail.com>
2016-04-15 12:50:01 +0000

    update package.json

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
 
 server.listen(app.get('port') ,app.get('ip'), function() {
-  console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+  console.log("Express server listening at %s:%d ", app.get('ip'),app.get('port'));
 });
 
 
